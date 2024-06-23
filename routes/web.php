2d03da2c3ba7a1b606a3d3b13cc5628c900457f5
wdbsa <wdbsamode@gmail.com>
2024-06-23 15:46:30 +0800

    components reusable

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+
+Route::get('/contact', function () {
+    return view('contact');
 });
